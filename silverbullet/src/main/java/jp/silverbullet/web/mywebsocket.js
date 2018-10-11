2be92c3287a107cdc932cdc31ae089924568ee83
copperbuleet <miyakemusic@yahoo.co.jp>
2018-10-11 13:34:34 +0900

    update

--- silverbullet/src/main/java/jp/silverbullet/web/mywebsocket.js
+++ silverbullet/src/main/java/jp/silverbullet/web/mywebsocket.js
@@ -1,9 +1,9 @@
 class MyWebSocket {
-	
+
 	constructor(callback, type) {
 		this.type = type;
 		////////// WebSocket //////////
-		var connection  = new WebSocket("ws://localhost:8081/websocket");
+		var connection  = new WebSocket("ws://" + window.location.host + ":8081/websocket");
 		// When the connection is open, send some data to the server
 		connection.onopen = function () {
 		};
