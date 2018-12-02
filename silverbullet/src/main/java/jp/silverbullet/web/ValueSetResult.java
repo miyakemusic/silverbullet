f371bcb5114bcc15376ef80da0ba3c341114eefb
copperbuleet <miyakemusic@yahoo.co.jp>
2018-12-03 00:20:57 +0900

    update

---
+++ silverbullet/src/main/java/jp/silverbullet/web/ValueSetResult.java
@@ -0,0 +1,10 @@
+package jp.silverbullet.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValueSetResult {
+	public List<String> debugLog = new ArrayList<>();
+	public String result = "";
+	
+}
