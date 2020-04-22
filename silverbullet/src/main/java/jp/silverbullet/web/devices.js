1b91d0421e590d4096cb6c849e3caabeff9667a7
yasuhiro <miyakemusic@yahoo.co.jp>
2020-04-23 00:29:08 +0900

    update

--- silverbullet/src/main/java/jp/silverbullet/web/devices.js
+++ silverbullet/src/main/java/jp/silverbullet/web/devices.js
@@ -2,6 +2,14 @@ class Devices {
 	constructor(div) {
 		var listId = div + "_deviceList";
 		$('#' + div).append('<div id="' + listId + '"></div>');
+		$('#' + listId).css({'display':'inline-block','width':'200px', 'border-width':'1px', 'border-color':'black'});
+ 		
+		var contentId = div + "_content";
+		$('#' + div).append('<div id="' + contentId + '">Main</div>');
+		$('#' + contentId).css({'display':'inline-block','width':'600px'});
+		
+		$('#' + div).append('<div id="' + listId + '"></div>');
+		
 		new MyWebSocket(function(msg) {
 			retreiveDevices();
 		}
@@ -17,7 +25,7 @@ class Devices {
 				success: function(msg){
 					$('#' + listId).empty();
 					for (var o of msg) {
-						$('#' + listId).append('<div><label>' + o + '</label></div>');
+						$('#' + listId).append('<div><button>' + o + '</button></div>');
 					}
 			   }
 			});
