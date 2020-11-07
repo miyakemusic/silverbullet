bd260ca3bec9423fb4b844612d7e3b81b568b1b6
yasuhiro <miyakemusic@yahoo.co.jp>
2020-11-08 01:52:37 +0900

    update

--- silverbullet/src/main/java/jp/silverbullet/swing/SbPanel.java
+++ silverbullet/src/main/java/jp/silverbullet/swing/SbPanel.java
@@ -1,17 +1,44 @@
 package jp.silverbullet.swing;
 
+import java.awt.Container;
+
 import javax.swing.JPanel;
 
 import jp.silverbullet.core.ui.UiModel;
+import jp.silverbullet.core.ui.UiProperty;
 import jp.silverbullet.core.ui.part2.Pane;
 
-public abstract class SbPanel {
+public abstract class SbPanel extends SbWidget{
 	abstract void processSubPane(Pane subPane);
 	
 	public SbPanel(Pane pane, UiModel uiModel, JPanel parent) {
+		super(pane, uiModel, parent);
 		for (Pane subPane : pane.widgets) {
 			processSubPane(subPane);
 		}
+	}
+
+	@Override
+	protected void onSize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onPosition(int left, int top) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void onInit(Pane pane, UiProperty uiProp, Container parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onUpdate(UiProperty uiProp) {
+		System.out.println();
 	}
 
 }
